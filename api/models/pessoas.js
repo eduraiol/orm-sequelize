35843b638312dadcefb1213361cd68b66d9023b5
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-12 10:43:17 -0300

    Usando validador personalizado no Modelo Pessoas

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -15,7 +15,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado){
+          if(dado.length < 3) throw new Error('Campo nome deve ter mais que 2 caracteres!')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: { 
       type: DataTypes.STRING,
